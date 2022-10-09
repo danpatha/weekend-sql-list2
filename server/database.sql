3a4ea0f91c44a00cdcdf14c69d706571770f0bdd
Daniel Pathammavong <patha037@umn.edu>
2022-10-09 17:43:35 -0500

    Update database.sql

--- server/database.sql
+++ server/database.sql
@@ -6,16 +6,18 @@ CREATE TABLE "list" (
 "id" SERIAL PRIMARY KEY,
 "task" VARCHAR (50) NOT NULL,
 "estimatedTimeInMin" INT,
-"dateDue" DATE, 
 "complete" BOOLEAN DEFAULT FALSE,
 "notes" VARCHAR (255));
 
 INSERT INTO "list" 
-("task","estimatedTimeInMin", "dateDue", "complete", "notes")
+("task","estimatedTimeInMin", "complete", "notes")
 VALUES
-('Grocery shopping', 60 , '10-12-2022', TRUE, 'I need to buy tacos to make for lunch!'),
-('Walk the dog', 45 , '10-09-2022', TRUE, 'Looking forward to getting outside.'),
-('Make lunch', 70 , '10-10-2022', FALSE, 'Play some music while you cook, it helps!'),
-('Clean the bathroom', 50 , '10-13-2022', FALSE, 'This will not be enjoyable, but it is necessary!'),
-('Takeout the trash and recycling', 2 , '10-08-2022', TRUE, 'Where does this stuff all go? Make a note to loop this up soon!'),
-('Go to the gym', 90 , '10-14-2022', FALSE, 'Rehabbing my knee and doing some upper body');
+('Grocery shopping', 60 , FALSE, 'I need to buy tacos to make for lunch!'),
+('Walk the dog', 45 , FALSE, 'Looking forward to getting outside.'),
+('Make lunch', 70 , FALSE, 'Play some music while you cook, it helps!'),
+('Clean the bathroom', 50 , FALSE, 'This will not be enjoyable, but it is necessary!'),
+('Takeout the trash and recycling', 2 , FALSE, 'Where does this stuff all go? Make a note to loop this up soon!'),
+('Go to the gym', 90 , FALSE, 'Rehabbing my knee and doing some upper body');
+
+
+
